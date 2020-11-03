0405259e0cda3670907222417b9b31ecaf5d268c
gsuuon <6422188+gsuuon@users.noreply.github.com>
2020-11-02 19:45:35 -0500

    Organize imports

--- rust-module/src/manipulate.rs
+++ rust-module/src/manipulate.rs
@@ -4,94 +4,27 @@ mod message;
 use flags::*;
 use message::*;
 
-use std::io::Error;
-use std::convert::TryInto;
-use std::ptr::null_mut;
-use std::mem;
+use winapi::{
+    shared::{
+        minwindef::{
+            BOOL,
+        },
+        windef::{
+            HWND,
+        }
+    },
+    um::winuser::{
+        SetLayeredWindowAttributes,
 
-use winapi::shared::minwindef::{
-    BOOL,
-};
+        GWL_EXSTYLE,
+        GWL_STYLE,
 
-use winapi::shared::windef::{
-    HWND,
-    RECT,
-};
+        LWA_ALPHA,
 
-use winapi::um::winuser::{
-    GetActiveWindow,
-    SetWindowLongW,
-    GetWindowLongW,
-    SetLayeredWindowAttributes,
-    GetWindowRect,
-    SetWindowPos,
-
-    GetMonitorInfoW,
-    MonitorFromWindow,
-
-    MONITORINFO,
-
-    GWL_EXSTYLE,
-    GWL_STYLE,
-
-    SWP_NOMOVE,
-    SWP_NOZORDER,
-    SWP_NOSIZE,
-    SWP_NOACTIVATE,
-
-    LWA_ALPHA,
-
-    MONITOR_DEFAULTTONEAREST,
-
-    WS_THICKFRAME,
-    WS_CAPTION,
-    WS_EX_LAYERED,
-};
-
-pub fn attempt(x: BOOL, msg: &str) -> BOOL {
-    if x == 0 {
-        print(&format!("Whoops.. Couldn't {}\n\n{}", msg, Error::last_os_error()));
+        WS_THICKFRAME,
+        WS_EX_LAYERED,
     }
-    x
-}
-
-pub fn remove_style(hwnd: HWND, style_flag: i32, style: u32) -> BOOL {
-    unsafe {
-        attempt(SetWindowLongW(
-                hwnd,
-                style_flag,
-                flags_remove(
-                    GetWindowLongW(hwnd, style_flag),
-                    style as i32)
-                )
-        , "add window style")
-    }
-}
-
-pub fn add_style(hwnd: HWND, style_flag: i32, style: u32) -> BOOL {
-    unsafe {
-        attempt(SetWindowLongW(
-                hwnd,
-                style_flag,
-                flags_add(
-                    GetWindowLongW(hwnd, style_flag),
-                    style as i32)
-                )
-        , "add window style")
-    }
-}
-
-pub fn get_style(hwnd: HWND, style_flag: i32) -> i32 {
-    unsafe { GetWindowLongW(hwnd, style_flag) }
-}
-
-pub fn remove_title(hwnd: HWND) -> BOOL {
-    attempt(remove_style(
-            hwnd,
-            GWL_STYLE,
-            WS_CAPTION)
-            , "set window style")
-}
+};
 
 pub fn enable_transparency(hwnd: HWND) -> BOOL {
     attempt(add_style(
@@ -118,69 +51,6 @@ pub fn set_opacity(hwnd: HWND, opacity: u8) -> BOOL {
         LWA_ALPHA
     )}, "set opacity")
 }
-
-pub fn set_window_rect(hwnd: HWND,
-                rect: RECT,
-                flags: u32) -> BOOL {
-    attempt(unsafe {
-        SetWindowPos(
-            hwnd,
-            null_mut(),
-            rect.left,
-            rect.top,
-            rect.right - rect.left,
-            rect.bottom - rect.top,
-            flags)
-    }, "set position")
-}
-
-pub fn get_window_rect(hwnd: HWND) -> Option<RECT>{
-    let mut rect : RECT = unsafe { mem::zeroed() };
-
-    if attempt(unsafe {
-        GetWindowRect(hwnd, &mut rect)
-    }, "get window rect") == 0 {
-        None
-    } else {
-        Some(rect)
-    }
-}
-
-pub fn get_monitor_rect(hwnd: HWND) -> Option<RECT> {
-    unsafe {
-        let monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-
-        let mut monitor_info : MONITORINFO = mem::zeroed();
-        monitor_info.cbSize = mem::size_of::<MONITORINFO>().try_into().unwrap();
-
-        if attempt(GetMonitorInfoW(
-                monitor,
-                &mut monitor_info)
-          , "get monitor info") == 0 {
-            None
-        } else {
-            Some(monitor_info.rcMonitor)
-        }
-    }
-}
-
-/// Calls SetWindowPos without changing to force redraw
-pub fn push_changes(hwnd: HWND) -> BOOL {
-    attempt(unsafe{SetWindowPos(
-        hwnd,
-        null_mut(),
-        0,
-        0,
-        0,
-        0,
-        SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOSIZE | SWP_NOMOVE
-    )}, "set window pos")
-}
-
-pub fn get_active_window() -> HWND {
-    unsafe{GetActiveWindow()}
-}
-
 pub fn hi() {
     let hwnd = get_active_window();
 
--- rust-module/src/manipulate/flags.rs
+++ rust-module/src/manipulate/flags.rs
@@ -1,7 +1,159 @@
-pub fn flags_add(source: i32, flags: i32) -> i32 {
+use crate::manipulate::message::print;
+
+use std::{
+    mem,
+    io::Error,
+    ptr::null_mut,
+    convert::TryInto
+};
+
+use winapi::{
+    shared::{
+        minwindef::{
+            BOOL,
+        },
+        windef::{
+            HWND,
+            RECT,
+        }
+    },
+    um::winuser::{
+        GetActiveWindow,
+        SetWindowLongW,
+        GetWindowLongW,
+        GetWindowRect,
+        SetWindowPos,
+
+        GetMonitorInfoW,
+        MonitorFromWindow,
+
+        MONITORINFO,
+
+        GWL_STYLE,
+
+        SWP_NOMOVE,
+        SWP_NOZORDER,
+        SWP_NOSIZE,
+        SWP_NOACTIVATE,
+
+        MONITOR_DEFAULTTONEAREST,
+
+        WS_CAPTION,
+    }
+};
+
+pub fn add_flags(source: i32, flags: i32) -> i32 {
     source | flags
 }
 
-pub fn flags_remove(source: i32, flags: i32) -> i32 {
+pub fn remove_flags(source: i32, flags: i32) -> i32 {
     source & (!flags)
 }
+
+pub fn attempt(x: BOOL, msg: &str) -> BOOL {
+    if x == 0 {
+        print(&format!("Whoops! Couldn't {}.\n\n{}", msg, Error::last_os_error()));
+    }
+    x
+}
+
+pub fn remove_style(hwnd: HWND, style_flag: i32, style: u32) -> BOOL {
+    attempt(unsafe {
+        SetWindowLongW(
+                hwnd,
+                style_flag,
+                remove_flags(
+                    GetWindowLongW(hwnd, style_flag),
+                    style as i32)
+                )
+        }, "add window style")
+}
+
+pub fn add_style(hwnd: HWND, style_flag: i32, style: u32) -> BOOL {
+    attempt(unsafe {
+        SetWindowLongW(
+                hwnd,
+                style_flag,
+                add_flags(
+                    GetWindowLongW(hwnd, style_flag),
+                    style as i32)
+                )
+        }, "add window style")
+    
+}
+
+pub fn get_style(hwnd: HWND, style_flag: i32) -> i32 {
+    unsafe { GetWindowLongW(hwnd, style_flag) }
+}
+
+pub fn remove_title(hwnd: HWND) -> BOOL {
+    attempt(remove_style(
+            hwnd,
+            GWL_STYLE,
+            WS_CAPTION)
+            , "set window style")
+}
+
+pub fn set_window_rect(hwnd: HWND,
+                rect: RECT,
+                flags: u32) -> BOOL {
+    attempt(unsafe {
+        SetWindowPos(
+            hwnd,
+            null_mut(),
+            rect.left,
+            rect.top,
+            rect.right - rect.left,
+            rect.bottom - rect.top,
+            flags)
+    }, "set position")
+}
+
+pub fn get_window_rect(hwnd: HWND) -> Option<RECT>{
+    let mut rect : RECT = unsafe { mem::zeroed() };
+
+    if attempt(unsafe {
+        GetWindowRect(hwnd, &mut rect)
+    }, "get window rect") == 0 {
+        None
+    } else {
+        Some(rect)
+    }
+}
+
+pub fn get_monitor_rect(hwnd: HWND) -> Option<RECT> {
+    unsafe {
+        let monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+        let mut monitor_info : MONITORINFO = mem::zeroed();
+        monitor_info.cbSize = mem::size_of::<MONITORINFO>().try_into().unwrap();
+
+        if attempt(GetMonitorInfoW(
+                monitor,
+                &mut monitor_info)
+          , "get monitor info") == 0 {
+            None
+        } else {
+            Some(monitor_info.rcMonitor)
+        }
+    }
+}
+
+/// Calls SetWindowPos without changes to force redraw
+pub fn push_changes(hwnd: HWND) -> BOOL {
+    attempt(unsafe {
+        SetWindowPos(
+            hwnd,
+            null_mut(),
+            0,
+            0,
+            0,
+            0,
+            SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOSIZE | SWP_NOMOVE
+            )}, "set window pos")
+}
+
+pub fn get_active_window() -> HWND {
+    unsafe { GetActiveWindow() }
+}
+
